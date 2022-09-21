a16a65c93acc2b79a84f53428d9adb8a49b18e56
VitorLamego <vitorlamegoo@gmail.com>
2022-09-21 16:01:26 -0300

    Modularizando EnergyMode

---
+++ main/energy-mode/include/energy_mode.h
@@ -0,0 +1,6 @@
+#ifndef ENERGY_MODE_H
+#define ENERGY_MODE_H
+
+void energyMode();
+
+#endif
--- main/main.c
+++ main/main.c
@@ -16,56 +16,7 @@
 #include "ky-036/include/ky-036.h"
 #include "ky-003/include/ky-003.h"
 #include "light-sleep/include/light-sleep.h"
-
-SemaphoreHandle_t conexaoWifiSemaphore;
-SemaphoreHandle_t conexaoMQTTSemaphore;
-
-void mqttTask(void *params)
-{
-    xTaskCreate(&conectadoWifi, "Conexão ao MQTT", 4096, NULL, 1, NULL);
-}
-
-void dht11Task(void *params)
-{
-    xTaskCreate(&tempUmidData, "Comunicação com Broker", 4096, NULL, 1, NULL);
-}
-
-void ledPWMTask(void *params)
-{
-    xTaskCreate(&ledHandle, "Configurando Led", 4096, NULL, 1, NULL);
-}
-
-void ky036Task(void *params)
-{
-    xTaskCreate(&touchHandle, "Sensor de Toque", 4096, NULL, 1, NULL);
-}
-
-void ky003Task(void *params)
-{
-    xTaskCreate(&digitalHallHandle, "Sensor Hall", 4096, NULL, 1, NULL);
-}
-
-void energyMode()
-{
-    conexaoWifiSemaphore = xSemaphoreCreateBinary();
-    conexaoMQTTSemaphore = xSemaphoreCreateBinary();
-    wifi_start();
-    gpioSetLed();
-
-    pthread_t tid[5];
-
-    pthread_create(&tid[0], NULL, (void *)mqttTask, (void *)NULL);
-    // pthread_create(&tid[1], NULL, (void *)dht11Task, (void *)NULL);
-    pthread_create(&tid[2], NULL, (void *)ledPWMTask, (void *)NULL);
-    pthread_create(&tid[3], NULL, (void *)ky036Task, (void *)NULL);
-    pthread_create(&tid[4], NULL, (void *)ky003Task, (void *)NULL);
-
-    pthread_join(tid[0], NULL);
-    // pthread_join(tid[1], NULL);
-    pthread_join(tid[2], NULL);
-    pthread_join(tid[3], NULL);
-    pthread_join(tid[4], NULL);
-}
+#include "energy-mode/include/energy_mode.h"
 
 void app_main(void)
 {
